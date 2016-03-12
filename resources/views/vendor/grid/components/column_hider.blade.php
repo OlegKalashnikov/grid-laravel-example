1d6518bcc6b55e9803cb60afc161c732632da6c9
Alexander Paramonov <xxxcoltxxx@gmail.com>
2016-03-13 02:32:47 +0300

    Update assets and views

--- resources/views/vendor/grid/components/column_hider.blade.php
+++ resources/views/vendor/grid/components/column_hider.blade.php
@@ -7,7 +7,6 @@
         data-dropdown-align-right="true"
         selectpicker
         ng-model="columns_hider"
-        ng-init="loadHider({{ json_encode(collect($columns)->where('default_column', true)->keys()->toArray() ?: $columns) }})"
 >
 
     @foreach($columns as $field => $column)
